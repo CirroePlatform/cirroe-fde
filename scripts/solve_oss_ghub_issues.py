3aa323341aed00cf7a36c8b575866a0f35f07c9f
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-11-09 11:27:04 -0800

    Added solving oss ghub issues script

--- scripts/solve_oss_ghub_issues.py
+++ scripts/solve_oss_ghub_issues.py
@@ -9,35 +9,35 @@ from src.integrations.kbs.issue_kb import IssueKnowledgeBase
 from src.integrations.kbs.documentation_kb import DocumentationKnowledgeBase
 from src.model.issue import Issue
 
-MEM0AI_ORG_ID = UUID("90a11a74-cfcf-4988-b97a-c4ab21edd0a1")
+from include.constants import MEM0AI_ORG_ID, MEM0AI_DOCU_URL
 
 
 # Setup repo, issue, and documentation knowledge bases
 async def setup_repos(org_id: UUID, repo_name: str):
     github = GithubIntegration(org_id, repo_name)
     issue_kb = IssueKnowledgeBase(org_id)
-    # doc_kb = DocumentationKnowledgeBase(org_id)
+    doc_kb = DocumentationKnowledgeBase(org_id)
 
     # github.index(Repository(remote="github", repository=repo_name, branch="main"))
 
-    issues = github.get_all_issues_json(repo_name, state=None)
-    for issue in issues:
+    # issues = github.get_all_issues_json(repo_name, state=None)
+    # for issue in issues:
 
-        comments = {}
-        for comment in issue["comments"]:
-            comments[comment["user"]["login"]] = comment["body"]
+    #     comments = {}
+    #     for comment in issue["comments"]:
+    #         comments[comment["user"]["login"]] = comment["body"]
 
-        #  Issue id is not a uuid, need to co
-        await issue_kb.index(
-            Issue(
-                primary_key=str(issue["id"]),
-                description=f"title: {issue['title']}, description: {issue['body']}",
-                comments=comments,
-                org_id=MEM0AI_ORG_ID,
-            )
-        )
+    #     #  Issue id is not a uuid, need to co
+    #     await issue_kb.index(
+    #         Issue(
+    #             primary_key=str(issue["id"]),
+    #             description=f"title: {issue['title']}, description: {issue['body']}",
+    #             comments=comments,
+    #             org_id=MEM0AI_ORG_ID,
+    #         )
+    #     )
 
-    # doc_kb.index()
+    await doc_kb.index(MEM0AI_DOCU_URL)
 
 
 def solve_issue(repo: str, issue_id: int):
--- src/model/documentation.py
+++ src/model/documentation.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
+from uuid import UUID
 
 class DocumentationPage(BaseModel):
+    primary_key: str # This ends up being a cryptographic hash, so str is fine.
     url: str
     content: str
